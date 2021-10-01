f0024a325e9d1cc9ce7fc0c0c868a3de0d64f538
Ezra <82878692+EzraBI@users.noreply.github.com>
2021-10-01 07:23:06 -0700

    Added 3rd batch of keywords

---
+++ typedef.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+//Typedef can be used to to giveS a name to the user defined data types
+//Below it defines a new data type and then use that data type to define 
+//structure variables directly
+typedef struct Books {
+   char title[50];
+   char author[50];
+   char subject[100];
+   int book_id;
+} Book;
+ 
+int main( ) {
+
+   Book book;
+ 
+   strcpy( book.title, "C Programming");
+   strcpy( book.author, "Nuha Ali"); 
+   strcpy( book.subject, "C Programming Tutorial");
+   book.book_id = 6495407;
+ 
+   printf( "Book title : %s\n", book.title);
+   printf( "Book author : %s\n", book.author);
+   printf( "Book subject : %s\n", book.subject);
+   printf( "Book book_id : %d\n", book.book_id);
+
+   return 0;
+}
